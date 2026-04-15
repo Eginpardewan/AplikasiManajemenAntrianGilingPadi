70594b1c9af63003edccb68e52d71fae7316f053
Egin Pardewan <eginpardewan@gmail.com>
2026-04-16 01:08:22 +0700

    Menambahkan koneksi database dan fungsi login

--- Giling_Padi/FormLogin.cs
+++ Giling_Padi/FormLogin.cs
@@ -1,20 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Giling_Padi;
+using System;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
-namespace Giling_Padi
+namespace AplikasiGilinganPadi
 {
-    public partial class Form1 : Form
+    public partial class FormLogin : Form
     {
-        public Form1()
+        private readonly string connectionString =
+            "Data Source=localhost;Initial Catalog=GilinganPadi;Integrated Security=True";
+
+        public FormLogin()
         {
             InitializeComponent();
+            txtPassword.PasswordChar = '*';
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text.Trim();
+            string password = txtPassword.Text;
+
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Email dan Password harus diisi!", "Peringatan",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "SELECT id_admin, nama FROM Admin WHERE email = @email AND password = @password";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@email", email);
+                    cmd.Parameters.AddWithValue("@password", password);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        int idAdmin = reader.GetInt32(0);
+                        string namaAdmin = reader.GetString(1);
+
+                        MessageBox.Show($"Selamat datang, {namaAdmin}!", "Login Berhasil",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        FormUtama formUtama = new FormUtama();
+                        formUtama.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Email atau password salah!", "Login Gagal",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
