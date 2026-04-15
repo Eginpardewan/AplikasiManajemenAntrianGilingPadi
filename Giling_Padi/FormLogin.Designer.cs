8eca1570a55e0afc23159c415483ba29c134e907
Egin Pardewan <eginpardewan@gmail.com>
2026-04-16 00:59:08 +0700

    Design: Mendesain UI FormLogin dengan input email dan password

--- Giling_Padi/FormLogin.Designer.cs
+++ Giling_Padi/FormLogin.Designer.cs
@@ -2,39 +2,86 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // label1 (Email)
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Text = "Email:";
+
+            // txtEmail
+            this.txtEmail.Location = new System.Drawing.Point(120, 47);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(200, 22);
+            this.txtEmail.Text = "admin@gmail.com";
+
+            // label2 (Password)
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.Text = "Password:";
+
+            // txtPassword
+            this.txtPassword.Location = new System.Drawing.Point(120, 87);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(200, 22);
+            this.txtPassword.PasswordChar = '*';
+
+            // btnLogin
+            this.btnLogin.Location = new System.Drawing.Point(120, 130);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(90, 30);
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+
+            // btnExit
+            this.btnExit.Location = new System.Drawing.Point(230, 130);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(90, 30);
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+
+            // FormLogin
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.Text = "Login - Aplikasi Gilingan Padi";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnExit);
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
-        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnExit;
     }
 }
-
