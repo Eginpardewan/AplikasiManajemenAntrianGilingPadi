4fb64989174a9d4747b9d68675c02a4c80acd2b3
Egin Pardewan <eginpardewan@gmail.com>
2026-04-16 01:14:44 +0700

    Update FormUtama.cs dengan Fungsi CRUD

---
+++ Giling_Padi/FormUtama.cs
@@ -0,0 +1,54 @@
+﻿using Giling_Padi;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace AplikasiGilinganPadi
+{
+    public partial class FormUtama : Form
+    {
+        private SqlConnection conn;
+        private readonly string connectionString =
+            "Data Source=localhost;Initial Catalog=GilinganPadi;Integrated Security=True";
+
+        public FormUtama()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                conn.Open();
+                string query = "SELECT id_antrian, nama_petani, alamat, no_telepon, berat_gabah, status FROM Antrian";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvAntrian.DataSource = dt;
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            FormAntrian form = new FormAntrian();
+            form.ShowDialog();
+            LoadData();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            FormLogin login = new FormLogin();
+            login.Show();
+            this.Close();
+        }
+    }
+}
