bd04022139d406b9776e6dc419b654c9dbe369a7
Egin Pardewan <eginpardewan@gmail.com>
2026-06-25 23:42:22 +0700

    Menambahkan Class Data untuk Crystal Report Hasil Giling

---
+++ Giling_Padi/ClassDataHasilGiling.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Giling_Padi
+{
+    public class ClassDataHasilGiling  // ← HARUS PUBLIC
+    {
+        public string NamaPetani { get; set; }
+        public string Alamat { get; set; }
+        public string NoTelepon { get; set; }
+        public int NoAntrian { get; set; }
+        public DateTime TanggalGiling { get; set; }
+        public decimal BeratGabah { get; set; }
+        public decimal Beras { get; set; }
+        public decimal Dedak { get; set; }
+        public string Status { get; set; }
+        public decimal KonversiBeras { get; set; }
+        public decimal KonversiDedak { get; set; }
+        public decimal SisaGabah { get; set; }
+        public string Periode { get; set; }
+        public int TotalPetani { get; set; }
+        public string TanggalCetak { get; set; }
+        public decimal TotalGabah { get; set; }
+        public decimal TotalBeras { get; set; }
+        public decimal TotalDedak { get; set; }
+    }
+}
